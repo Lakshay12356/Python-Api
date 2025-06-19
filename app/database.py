6c121d175db1e096fa362eaf595f55b90df0d6a5
Lakshay Jain <131363148+Lakshay12356@users.noreply.github.com>
2025-06-19 04:28:45 +0000

    database integrate

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Railway or local PostgreSQL connection
-SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<host>:<port>/<database>"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ims_databse_user:LLMbldmtRJG8QmMQJBmCqRgEy24GovEg@dpg-d19p3ammcj7s73eninfg-a/ims_databse"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
